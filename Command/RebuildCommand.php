9ad6dfc1047073a433abd114c31261ac43a20f29
jmsche <jonathan.scheiber@siep.be>
2017-07-25 09:35:13 +0200

    Symfony 3.3 Autowiring: Command / Fixed some services autowiring

--- Command/RebuildCommand.php
+++ Command/RebuildCommand.php
@@ -2,7 +2,8 @@
 
 namespace Leapt\ElasticaBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Leapt\ElasticaBundle\Service;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,8 +12,11 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Class RebuildCommand
  * @package Leapt\ElasticaBundle\Command
  */
-class RebuildCommand extends ContainerAwareCommand
+class RebuildCommand extends Command
 {
+    /** @var Service */
+    protected $elastica;
+
     protected function configure()
     {
         $this
@@ -30,24 +34,32 @@ class RebuildCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $timeStart = microtime(true);
-        $elastica = $this->getContainer()->get('leapt_elastica.service');
 
         // Rebuild only given types
         if ($input->hasArgument('types') && 0 < count($input->getArgument('types'))) {
             foreach ($input->getArgument('types') as $type) {
                 $output->writeln(sprintf('Rebuilding "%s" type', $type));
-                $elastica->rebuildType($type);
+                $this->elastica->rebuildType($type);
             }
         }
 
         // Rebuild all types
         else {
             $output->writeln('Rebuilding all elastica indexes');
-            $elastica->createIndexes();
-            $elastica->reindex();
+            $this->elastica->createIndexes();
+            $this->elastica->reindex();
         }
 
         $time = number_format(microtime(true) - $timeStart, 3);
         $output->writeln('Rebuilt indexes in ' . $time . ' seconds.');
+    }
+
+    /**
+     * @param Service $elastica
+     * @required
+     */
+    public function setElasticaService(Service $elastica): void
+    {
+        $this->elastica = $elastica;
     }
 }
