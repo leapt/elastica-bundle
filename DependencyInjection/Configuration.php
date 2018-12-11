f9d591ea69b1b67f0f1f9957a5802e7686dc9a69
jmsche <jonathan.scheiber@siep.be>
2018-12-11 08:35:36 +0100

    Fixed DependencyInjection deprecation for Symfony 4.2 (includes BC layer)

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,14 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('leapt_elastica');
+        $treeBuilder = new TreeBuilder('leapt_elastica');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('leapt_elastica');
+        }
+
         $rootNode
             ->children()
                 ->arrayNode('config')
